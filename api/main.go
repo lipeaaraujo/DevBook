4c610d4f1b0d3e68559de37125dada95f08eb7e1
lipeaaraujo <felipearaujodff@gmail.com>
2026-08-02 01:51:41 -0300

    feat: adding cors config

--- api/main.go
+++ api/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"api/src/config"
+	"api/src/middlewares"
 	"api/src/router"
+
 	"fmt"
 	"log"
 	"net/http"
@@ -14,8 +16,10 @@ func main() {
 	fmt.Println("Running API on port", config.Port)
 	r := router.Generate()
 
+	handler := middlewares.Cors(r)
+
 	portStr := fmt.Sprintf(":%d", config.Port)
-	err := http.ListenAndServe(portStr, r)
+	err := http.ListenAndServe(portStr, handler)
 	if err != nil {
 		log.Fatal(err)
 	}
--- api/src/middlewares/middlewares.go
+++ api/src/middlewares/middlewares.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"api/src/apierrors"
+	"api/src/config"
 	"api/src/responses"
 	"api/src/utils/auth"
 	"log"
@@ -23,4 +24,22 @@ func Auth(next http.HandlerFunc) http.HandlerFunc {
 		}
 		next(w, r)
 	}
+}
+
+func Cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", config.FrontendURL)
+		w.Header().Set("Vary", "Origin")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Max-Age", "86400")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
